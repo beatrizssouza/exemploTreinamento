52b6af666fb0b3225d99bd4d23a62394411bb011
Beatriz Silva Souza <msfemacell@gmail.com>
2021-09-29 19:02:35 -0300

    adicionando um novo componente e fazendo um menu

--- workspace/primeiro-projeto/src/App.vue
+++ workspace/primeiro-projeto/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld />
+    <NovoComponente />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import NovoComponente from "./components/NovoComponente.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    NovoComponente,
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  background-color: white;
 }
 </style>
---
+++ workspace/primeiro-projeto/src/components/NovoComponente.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="menu">
+    <p>projeto</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NovoComponente",
+};
+</script>
+
+<style scoped>
+.menu {
+  max-width: 100%;
+  background-color: slategray;
+}
+</style>
